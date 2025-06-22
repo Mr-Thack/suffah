3edbdd41abc2cf9e284e354c1ed830a410e900ae
AbdulMuqeet Mohammed <abdulmuqeet.mohammed@yahoo.com>
2025-06-21 20:30:54 -0400

    Init Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+
+  import { ModeWatcher } from "mode-watcher";
+  import NavBar from "$lib/components/Navbar.svelte";  
+
+	let { children } = $props();
+</script>
+
+
+<header class="w-full border-b">
+  <div class="flex h-14 items-center px-4">
+    <NavBar />
+  </div>
+</header>
+  
+<!--
+<ModeWatcher />
+-->
+{@render children()}
